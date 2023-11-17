d4f27e962b64bc1ad0897b276efa1a6031f904dc
fudaohui <fudaohui@hozonauto.com>
2023-11-17 18:11:41 +0800

    mysql数据插入ok

--- app/blueprints/user.py
+++ app/blueprints/user.py
@@ -2,15 +2,27 @@ import logging
 
 from flask import Blueprint, request
 
+from ..models.module import Student
 from ..utils.helpers import generate_response
+from ..extensions import db
 
 user_blueprint = Blueprint("user", __name__, url_prefix="/user")
 
-
 logger = logging.getLogger(__name__)
+
 
 @user_blueprint.route('/create_user', methods=['POST'])
 def add_user():
     json = request.get_json()
     logger.info(f"添加用户，接收的参数为：{json}")
+    name = json.get('name')
+    if not name:
+        return generate_response(500, "用户名为空")
+    student = Student(name=name)
+    try:
+        db.session.add(student)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return generate_response(500, "回滚啦")
     return generate_response(200, "成功")
--- app/models/module.py
+++ app/models/module.py
@@ -1,10 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
+
+
 class Student(db.Model):
-    __tablename__ = 'student'
+    __tablename__  = 'student'
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
-
-
